98077a458a526a5351452eabcc1bbcc837f1d9b5
simoZak <zakhnini92@gmail.com>
2023-03-21 16:16:36 -0700

    task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - list all natural numbers multiples of 3 & 5
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	int num, sum;
+	long int sum;
+	long int first, second;
+	int count;
 
 	sum = 0;
+	first = 0;
+	second = 1;
+	count = 0;
 
-	for (num = 0; num < 1024; num++)
+	while (count < 49)
 	{
-		if (num % 3 == 0 || num % 5 == 0)
-		{
-			sum += num;
-		}
-	}
+		sum = first + second;
+		printf("%li, ", sum);
+		first = second;
+		second = sum;
 
-	printf("%d\n", sum);
+		count++;
+	}
+	sum = first + second;
+	printf("%li\n", sum);
 
 	return (0);
 }
