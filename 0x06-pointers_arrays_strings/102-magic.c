52d021320d65c2ad8aad4f8047177094710268dc
simoZak <zakhnini92@gmail.com>
2023-03-31 16:12:26 -0700

    task 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-/*
-* main - Entry point
-*
-* Description: 'the program's description'
-* @parameter: describe the parameter
-*
-* Return: Always 0 (Success)
-*/
+/**
+ * main - Entry point;
+ * Return: Always 0 (Success);
+ */
 
 int main(void)
 {
