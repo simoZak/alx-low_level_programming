1b116a583db30013424d5959f6e4722390075f13
simoZak <zakhnini92@gmail.com>
2023-04-05 17:47:20 -0700

    task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,32 +14,32 @@ int wildcmp(char *s1, char *s2)
 	else if (*s1 == '\0' || *s2 == '\0')
 	{
 		if (*s1 == '\0' && *s2 == '*')
-			return wildcmp(s1, ++s2);
+			return (wildcmp(s1, ++s2));
 		else if (*s1 == '*' && *s2 == '\0')
-			return wildcmp(++s1, s2);
+			return (wildcmp(++s1, s2));
 		return (0);
 	}
 
 	if (*s1 == *s2)
 	{
-		return wildcmp(++s1, ++s2);
+		return (wildcmp(++s1, ++s2));
 	}
 	else if (*s1 == '*')
 	{
 		if (*(s1 + 1) == '*')
-			return wildcmp(++s1, s2);
+			return (wildcmp(++s1, s2));
 		else
 		{
-			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
+			return (wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2));
 		}
 	}
 	else if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
-			return wildcmp(s1, ++s2);
+			return (wildcmp(s1, ++s2));
 		else
 		{
-			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
+			return (wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2));
 		}
 	}
 
