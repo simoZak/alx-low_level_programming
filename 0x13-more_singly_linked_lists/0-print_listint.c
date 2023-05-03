1948fa75da5d7f2390be67e97d5401afcb62adef
simoZak <zakhnini92@gmail.com>
2023-05-03 02:05:10 +0100

    task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,17 +5,15 @@
  *   * @h: linked list of type listint_t to print
  *    *
  *     * Return: number of nodes
- *      */
+ */
 size_t print_listint(const listint_t *h)
 {
-		size_t num = 0;
-
-			while (h)
-					{
-								printf("%d\n", h->n);
-										num++;
-												h = h->next;
-													}
-
-				return (num);
+size_t num = 0;
+while (h)
+{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+}
+return (num);
 }
