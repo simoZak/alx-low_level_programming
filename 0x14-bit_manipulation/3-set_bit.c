0433184ef294e4ff55a7b9c124bb36571ad2798c
simoZak <zakhnini92@gmail.com>
2023-05-05 01:59:59 +0100

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,17 +5,14 @@
  *   * @n: decimal number passed by pointer
  *    * @index: index position to change, starting from 0
  *     * Return: 1 if it worked, -1 if error
- *      */
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-		unsigned long int p;
-
-			if (index > 64)
-						return (-1);
-
-				for (p = 1; index > 0; index--, p *= 2)
-							;
-					*n += p;
-
-						return (1);
+unsigned long int p;
+if (index > 64)
+	return (-1);
+for (p = 1; index > 0; index--, p *= 2)
+	;
+*n += p;
+return (1);
 }
