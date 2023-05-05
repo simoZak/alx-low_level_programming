35b5c5f8454d73bafaa8bd33c7aaa5c6b0ab03c7
simoZak <zakhnini92@gmail.com>
2023-05-05 02:03:06 +0100

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,20 +6,17 @@
  *   * @n: first number
  *    * @m: second number to convert to
  *     * Return: number of bits that was needed to flip
- *      */
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-		unsigned long int diff;
-			int counter;
-
-				diff = n ^ m;
-					counter = 0;
-
-						while (diff)
-								{
-											counter++;
-													diff &= (diff - 1);
-														}
-
-							return (counter);
+unsigned long int diff;
+int counter;
+diff = n ^ m;
+counter = 0;
+while (diff)
+{
+	counter++;
+	diff &= (diff - 1);
+}
+return (counter);
 }
