826b34922168453255e3c20315d139280021893e
simoZak <zakhnini92@gmail.com>
2023-05-05 01:58:38 +0100

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,15 +5,12 @@
  *   * @n: number to evaluate
  *    * @index: index starting from 0, of the bit we want to get
  *     * Return: Value of bit at index, or -1 if error
- *      */
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-		unsigned long int hold;
-
-			if (index > 64)
-						return (-1);
-
-				hold = n >> index;
-
-					return (hold & 1);
+unsigned long int hold;
+if (index > 64)
+	return (-1);
+hold = n >> index;
+return (hold & 1);
 }
