1440eb4a9289303019b085820ef44d4a7c09b060
simoZak <zakhnini92@gmail.com>
2024-03-22 00:54:17 +0000

    betty coding styl corretion

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,7 +7,7 @@
  *     * @head: head of the list
  *      * @n: value of the element
  *       * Return: the address of the new element
- *        */
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,7 +7,7 @@
  *     * @head: head of the list
  *      * @index: index of the nth node
  *       * Return: nth node
- *        */
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
